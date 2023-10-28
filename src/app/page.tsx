2147edbd8421aee29bdf414b0bdff249bb732e0e
superbuggy <jpqreichard@gmail.com>
2023-10-27 22:18:07 -0400

    💄 Change main layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,23 @@
-import Image from "next/image";
 import styles from "./page.module.css";
-import WavePlayer from '../components/WavePlayer'
+import WavePlayer from "../components/WavePlayer";
 
 export default function Home() {
   return (
-    <main>
+    <>
       <h1 className={styles.center}>Demnote</h1>
-      <WavePlayer/>
-    </main>
+      <main className={styles.main}>
+        <aside className={styles["main-aside"]}>
+          Comments:
+          <ul>
+            <li>Comment #1</li>
+            <li>Comment #2</li>
+            <li>Comment #3</li>
+          </ul>
+        </aside>
+        <section className={styles["main-player"]}>
+          <WavePlayer />
+        </section>
+      </main>
+    </>
   );
 }
