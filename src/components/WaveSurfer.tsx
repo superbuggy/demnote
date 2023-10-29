64769044a21e86aeb85131d5e5d4ae33bbdbca14
superbuggy <jpqreichard@gmail.com>
2023-10-29 00:15:14 -0400

    🚧 Prototype annotation timeline

--- src/components/WaveSurfer.tsx
+++ src/components/WaveSurfer.tsx
@@ -7,9 +7,12 @@ const { useRef, useState, useEffect, useCallback } = React;
 import WaveSurfer from "wavesurfer.js";
 
 const formatSeconds = (durationInSeconds: number): string => {
-  const minutesExact = durationInSeconds/60;
+  const minutesExact = durationInSeconds / 60;
   const minutes = Math.floor(minutesExact);
-  const seconds = `${Math.round(60 * (minutesExact - minutes))}`.padStart(2, '0');
+  const seconds = `${Math.round(60 * (minutesExact - minutes))}`.padStart(
+    2,
+    "0"
+  );
   return `${minutes}:${seconds}`;
 };
 
@@ -22,15 +25,15 @@ const useWaveSurfer = (containerRef, options) => {
   useEffect(() => {
     if (!containerRef.current) return;
 
-    const ws = WaveSurfer.create({
+    const waveSurferInstance = WaveSurfer.create({
       ...options,
       container: containerRef.current,
     });
 
-    setWaveSurfer(ws);
+    setWaveSurfer(waveSurferInstance);
 
     return () => {
-      ws.destroy();
+      waveSurferInstance.destroy();
     };
   }, [options, containerRef]);
 
@@ -44,6 +47,7 @@ export const WaveSurferPlayer = (props) => {
   const [hasLoaded, setHasLoaded] = useState(false);
   const [isPlaying, setIsPlaying] = useState(false);
   const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(0);
   const waveSurfer = useWaveSurfer(containerRef, props);
 
   // On play button click
@@ -60,7 +64,10 @@ export const WaveSurferPlayer = (props) => {
     setIsPlaying(false);
 
     const subscriptions = [
-      waveSurfer.on("ready", () => setHasLoaded(true)),
+      waveSurfer.on("ready", () => {
+        setHasLoaded(true);
+        setDuration(waveSurfer.decodedData.duration);
+      }),
       waveSurfer.on("play", () => setIsPlaying(true)),
       waveSurfer.on("pause", () => setIsPlaying(false)),
       waveSurfer.on("timeupdate", (currentTime) => setCurrentTime(currentTime)),
@@ -78,7 +85,9 @@ export const WaveSurferPlayer = (props) => {
         style={{ minHeight: "120px", display: hasLoaded ? "block" : "none" }}
       />
       {!hasLoaded && <h1>Loading...</h1>}
-
+      <svg viewBox={`0 0 ${duration} 48`} width="100%" height="48px">
+        <circle cx={duration/2} cy={48/2} r="20" fill="#FFF" />
+      </svg>
       <button onClick={onPlayClick} style={{ marginTop: "1em" }}>
         {isPlaying ? "Pause" : "Play"}
       </button>
