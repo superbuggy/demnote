f333c16b0bdd35500fc41069f5f7953980221883
superbuggy <jpqreichard@gmail.com>
2023-10-30 00:27:07 -0400

    ✨ Use data from endpoints

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,67 @@
+"use client";
+
 import styles from "./page.module.css";
 import WavePlayer from "../components/WavePlayer";
+import Comments from "../components/CommentList";
+import CommentDetails from "../components/CommentDetails";
+import { useEffect, useState, useCallback } from "react";
+import { AudioFile, Comment } from "../types";
 
 export default function Home() {
+  const [audioFile, setAudioFile] = useState<AudioFile>();
+  const [audioFiles, setAudioFiles] = useState<AudioFile[]>([]);
+  const [shownComment, setShownComment] = useState<Comment>();
+
+  const onUrlChange = useCallback(
+    (event) => {
+      const nextAudioFile = audioFiles.find(
+        ({ url }) => url === event.target.value
+      );
+      if (nextAudioFile) setAudioFile(nextAudioFile);
+      if (!nextAudioFile) console.error("onUrlChange error", event);
+    },
+    [audioFiles]
+  );
+
+  const showComment = useCallback(
+    (id) => {
+      const commentToShow = audioFile?.comments.find(
+        (comment) => id === comment.id
+      );
+      if (commentToShow) setShownComment(commentToShow);
+    },
+    [audioFile?.comments]
+  );
+
+  useEffect(() => {
+    async function fetchAudioFiles() {
+      const response = await fetch(`/api/audiofile`);
+      const json = await response.json();
+      setAudioFiles(() => json);
+      setAudioFile(() => json[1]);
+    }
+
+    fetchAudioFiles().catch(console.error);
+  }, []);
   return (
     <>
       <h1 className={styles.center}>Demnote</h1>
-      <main className={styles.main}>
-        <aside className={styles["main-aside"]}>
-          Comments:
-          <ul>
-            <li>Comment #1</li>
-            <li>Comment #2</li>
-            <li>Comment #3</li>
-          </ul>
-        </aside>
-        <section className={styles["main-player"]}>
-          <WavePlayer />
-        </section>
-      </main>
+      {audioFile && (
+        <main className={styles.main}>
+          <Comments comments={audioFile.comments} showComment={showComment} />
+          <section className={styles["main-player"]}>
+            <select onChange={onUrlChange} value={audioFile.url}>
+              {audioFiles.map(({ name, url }) => (
+                <option key={name} value={url}>
+                  {name}
+                </option>
+              ))}
+            </select>
+            <WavePlayer audioUrl={audioFile.url} />
+          </section>
+          <CommentDetails shownComment={shownComment} />
+        </main>
+      )}
     </>
   );
 }
