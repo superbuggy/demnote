329e8a9b7627ee948b8b81d3dc48e8c4f840327a
superbuggy <jpqreichard@gmail.com>
2023-11-07 21:45:49 -0500

    💄 Pad comment content

--- src/components/CommentDetails.tsx
+++ src/components/CommentDetails.tsx
@@ -1,14 +1,38 @@
 export default function CommentDetails(props) {
-  const { shownComment } = props;
+  const { shownComment, isInTooltip } = props;
 
   return (
     shownComment && (
       <section>
         <h2>{shownComment.title}</h2>
-        <p>{shownComment.author.name}</p>
-        <p>{shownComment.text}</p>
-        <button>Edit</button>
-        <button>Delete</button>
+        <p className="byline">{shownComment.author.name}</p>
+        <p className="comment-text">{shownComment.text}</p>
+        <nav>
+          <button>Edit Text</button>
+          <button>Change Selection</button>
+          <button>Delete</button>
+        </nav>
+        <style jsx>{`
+          p.byline {
+            margin-bottom: 1rem;
+            font-size: 0.75rem;
+            font-style: italic;
+          }
+          p.comment-text {
+            font-size: 0.75rem;
+            max-width: 400px;
+          }
+          nav {
+            margin-top: 1rem;
+          }
+          button:not(:last-of-type) {
+            margin-right: 0.5rem;
+          }
+
+          section {
+            padding: 1rem;
+          }
+        `}</style>
       </section>
     )
   );
